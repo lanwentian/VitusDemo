e74073e2532a5cc8848f4e1d57b780bafb71bea2
lanwentian <lanwentian@6clue.com>
2019-01-31 13:47:25 +0800

    teset

---
+++ testFramework/VitusPerson.h
@@ -0,0 +1,17 @@
+//
+//  VitusPerson.h
+//  testDemo
+//
+//  Created by LANWENTIAN on 2019/1/31.
+//  Copyright © 2019年 LANWENTIAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VitusPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testFramework/VitusPerson.m
@@ -0,0 +1,13 @@
+//
+//  VitusPerson.m
+//  testDemo
+//
+//  Created by LANWENTIAN on 2019/1/31.
+//  Copyright © 2019年 LANWENTIAN. All rights reserved.
+//
+
+#import "VitusPerson.h"
+
+@implementation VitusPerson
+
+@end
